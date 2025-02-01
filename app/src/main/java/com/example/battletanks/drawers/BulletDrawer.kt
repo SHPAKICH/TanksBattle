fd83e18d3c124da2a200333bb9f5b69dd01eb85c
SHPAKICH <krabic2091@mail.ru>
2025-02-01 18:26:12 +0300

    0.10

--- app/src/main/java/com/example/battletanks/drawers/BulletDrawer.kt
+++ app/src/main/java/com/example/battletanks/drawers/BulletDrawer.kt
@@ -81,26 +81,32 @@ class BulletDrawer(val container: FrameLayout) {
         elementsOnContainer: MutableList<Element>,
         detectedCoordinatesList: List<Coordinate>
     ) {
-        if (checkContainerContainsElements(
-                elementsOnContainer.map { it.coordinate },
-                detectedCoordinatesList
-            )
-        ) {
             detectedCoordinatesList.forEach {
                 val element = getElementByCoordinates(it, elementsOnContainer)
                 removeElementsAndStopBullet(element, elementsOnContainer)
             }
-        }
-
     }
 
     private fun removeElementsAndStopBullet(
         element: Element?,
         elementsOnContainer: MutableList<Element>
     ) {
+        if (element != null) {
+            if (element.material.bulletCanGoThrough){
+                return
+            }
+            if  (element.material.simpleBulletCanDestroy){
+                stopBullet()
+                removeView(element)
+                elementsOnContainer.remove(element)
+            } else {
+                stopBullet()
+            }
+        }
+    }
+
+    private fun stopBullet() {
         canBulletGoFurther = false
-        removeView(element)
-        elementsOnContainer.remove(element)
     }
 
     private fun removeView(element: Element?) {
@@ -110,18 +116,6 @@ class BulletDrawer(val container: FrameLayout) {
             container.removeView(activity.findViewById(element.viewId))
             }
         }
-    }
-
-    private fun checkContainerContainsElements(
-        elementsOnContainer: List<Coordinate>,
-        detectedCoordinatesList: List<Coordinate>
-    ): Boolean {
-        detectedCoordinatesList.forEach {
-            if (elementsOnContainer.contains(it)) {
-                return true
-            }
-        }
-        return false
     }
 
     private fun getCoordinatesForTopOrBottomDirection(bulletCoordinate: Coordinate): List<Coordinate>{
